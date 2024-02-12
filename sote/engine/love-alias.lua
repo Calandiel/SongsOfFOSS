4d94ec3ca1bdac509e9e8b744a3d0386d4f89aa4
squealingpiggies <squealingpiggies@gmail.com>
2024-02-12 09:09:03 -0800

    added alias and type defs in love-alias.lua to clean up warnings

---
+++ sote/engine/love-alias.lua
@@ -0,0 +1,25 @@
+---@alias love.KeyConstant characterKeys | symbolKeys | numpadKeys | navigationKeys | editingKeys | functionKeys | modifierKeys | applicationKeys | miscKeys
+---@alias characterKeys "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+---@alias symbolKeys "!" | "\"" | "#" | "$" | "&" | "\'" | "(" | ")" | "*" | "+" | "," | "-" | "." | "/" | ":" | ";" | "<" | "=" | ">" | "?" | "@" | "\[" | "\\" | "\]" | "^" | "_" | "`"
+---@alias numpadKeys "kp0" | "kp1" | "kp2" | "kp3" | "kp4" | "kp5" | "kp6" | "kp7" | "kp8" | "kp9" | "kp." | "kp," | "kp/" | "kp*" | "kp+" | "kpenter" | "kp="
+---@alias navigationKeys "up" | "down" | "right" | "left" | "home" | "end" | "pageup" | "pagedown"
+---@alias editingKeys "insert" | "backspace" | "tab" | "clear" | "return" | "delete"
+---@alias functionKeys "f1" | "f2" | "f3" | "f4" | "f5" | "f6" | "f7" | "f8" | "f9" | "f10" | "f11" | "f12" | "f13" | "f14" | "f15" | "f16" | "f17" | "f18"
+---@alias applicationKeys "www" | "mail" | "calculator" | "computer" | "appsearch" | "apphome" | "appback" | "appforward" | "apprefresh" | "appbookmarks"
+---@alias modifierKeys "numlock" | "capslock" | "scrolllock" | "rshift" | "lshift" | "rctrl" | "lctrl" | "ralt" | "lalt" | "rgui" | "lgui" | "mode"
+---@alias miscKeys "pause" | "escape" | "help" | "printscreen" | "sysreq" | "menu" | "application" | "power" | "currencyunit" | "undo"
+
+---@class love.Image
+---@field getWidth fun():number
+---@field getHeight fun():number
+
+
+---@class FileData
+
+---@alias ImageEncodingFormat "tga" | "png" | "exr" | "jpg" | "bmp"
+---@class love.ImageData
+---@field encode fun(self: love.ImageData, foramt: ImageEncodingFormat, fielname: string?): FileData
+
+
+---@alias love.Source.stop fun():boolean
+---@alias love.Source.play fun():boolean
