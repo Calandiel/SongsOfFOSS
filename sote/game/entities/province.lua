994cdd4591112068d66c9abce6f5f41cd0846316
squealingpiggies <squealingpiggies@gmail.com>
2024-02-12 15:41:10 -0800

    fixed transfer_pop

--- sote/game/entities/province.lua
+++ sote/game/entities/province.lua
@@ -300,6 +300,14 @@ function prov.Province:transfer_pop(pop, target)
 	target.all_pops[pop] = pop
 
 	pop.province = target
+
+	local children = tabb.filter(pop.children, function(c)
+		return self.all_pops[c] and c.home_province ~= self
+			and not c.unit_of_warband and not c.employer
+	end)
+	for _,c in pairs(children) do
+		self:transfer_pop(c,target)
+	end
 end
 
 --- Changes home province of a pop/character to the target province
