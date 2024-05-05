a5b7e4e9a7e3c3a2e1d8a36284c94f3c5fce961a
dsecrieru <danysecrieru@yahoo.com>
2024-05-05 22:18:03 +0200

    bugfixing inital waterbodies gen and minor refactor

--- sote/libsote/hydrology/gen-initial-waterbodies.lua
+++ sote/libsote/hydrology/gen-initial-waterbodies.lua
@@ -6,10 +6,9 @@ local waterbodies_created = 0
 
 local function process(tile_index, world)
 	local is_land = world:get_is_land_by_index(tile_index)
-	if not is_land then return end
+	if is_land then return end
 
-	local waterbody_is_valid = world:is_waterbody_valid(tile_index)
-	if waterbody_is_valid then return end
+	if world:is_waterbody_valid(tile_index) then return end
 
 	local new_waterbody_id = world:create_new_waterbody()
 	waterbodies_created = waterbodies_created + 1
@@ -22,10 +21,9 @@ local function process(tile_index, world)
 
 		world:for_each_neighbor(current_tile_index, function(ni)
 			local neighbor_is_land = world:get_is_land_by_index(ni)
-			if not neighbor_is_land then return end
+			if neighbor_is_land then return end
 
-			local neighbor_waterbody_is_valid = world:is_waterbody_valid(ni)
-			if neighbor_waterbody_is_valid then return end
+			if world:is_waterbody_valid(ni) then return end
 
 			world.waterbody_by_tile[ni] = new_waterbody_id
 			queue:enqueue(ni)
