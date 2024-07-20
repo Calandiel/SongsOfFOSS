2bc6565cea68162c88555901fd43c62dff96f44c
dsecrieru <danysecrieru@yahoo.com>
2024-07-20 14:07:20 +0200

    find glacial perimeter

--- sote/libsote/glacial-formation.lua
+++ sote/libsote/glacial-formation.lua
@@ -9,7 +9,7 @@ local AGE_TYPES = {
 	game_age = 1
 }
 
-local logger = require("libsote.debug-loggers").get_glacial_logger("d:/temp")
+-- local logger = require("libsote.debug-loggers").get_glacial_logger("d:/temp")
 local open_issues = require "libsote.glacial-open-issues"
 
 local glacial_seed = nil
@@ -17,16 +17,23 @@ local ice_flow = nil
 local ice_moved = nil
 local distance_from_edge = nil
 
+local old_layer = {}
+local new_layer = {}
+local glacial_seed_table = {}
+
 local function create_glacial_start_locations(world, is_ice_age)
+
 	local ice_age_factor = 5
 	if is_ice_age then ice_age_factor = -10 end --* Makes world colder for ice age
 
 	local seed_threshold = -15 --* -15 for non-Ice Age
 	if is_ice_age then seed_threshold = 0 end
 
+	local start = love.timer.getTime()
+
 	world:for_each_tile(function(ti)
 	-- world:for_each_tile_by_elevation_for_waterflow(function(ti, _)
-		-- local log_str = "gs: " .. world.colatitude[ti] .. "," .. world.minus_longitude[ti] .. "; " .. world:true_elevation_for_waterflow(ti) .. "\n"
+		-- local log_str = "gs: " .. world.colatitude[ti] .. "," .. world.minus_longitude[ti] .. "; " .. world:true_elevation_for_waterflow(ti) .. "; " .. ti .. "\n"
 
 		local wb = world:get_waterbody_by_tile(ti)
 		local waterbody_size = (wb == nil) and 0 or #wb.tiles
@@ -43,17 +50,91 @@ local function create_glacial_start_locations(world, is_ice_age)
 			-- logger:log(log_str)
 			return
 		end
-		-- log_str = log_str .. "\tis seeded"
 
 		local ice_depth = average_temp * -0.25 * open_issues.rainfall_contrib_to_ice_depth(jan_rainfall, jul_rainfall)
 		if ice_depth > 0 then
-			glacial_seed[ti] = true
+			-- log_str = log_str .. "\tis seeded"
+			-- glacial_seed[ti] = true
+			glacial_seed_table[ti] = true
 			ice_flow[ti] = ice_depth
-		else
-			glacial_seed[ti] = false
+		-- else
+			-- log_str = log_str .. "\tnot seeded, no ice"
+			-- glacial_seed[ti] = false
 		end
 		-- logger:log(log_str)
 	end)
+
+	local duration = love.timer.getTime() - start
+	print("[glacial-formation] create_glacial_start_locations: " .. tostring(duration * 1000) .. "ms")
+
+	local count = 0
+	-- world:for_each_tile(function(ti)
+	-- 	if glacial_seed[ti] then
+	-- 		count = count + 1
+	-- 	end
+	-- end)
+	for _ in pairs(glacial_seed_table) do
+		count = count + 1
+	end
+	print("Glacial Seeds: " .. count)
+end
+
+local function find_glacial_perimeter(world)
+	old_layer = {}
+
+	local start = love.timer.getTime()
+
+	-- world:for_each_tile(function(ti)
+	-- 	if not glacial_seed[ti] then return end
+
+	-- 	local has_seed_neighbour = false
+	-- 	world:for_each_neighbor(ti, function(nti)
+	-- 		if glacial_seed[nti] then return end
+	-- 		has_seed_neighbour = true
+	-- 	end)
+
+	-- 	if has_seed_neighbour then
+	-- 		old_layer[ti] = true
+	-- 		distance_from_edge[ti] = 1
+	-- 	end
+	-- end)
+	for ti, _ in pairs(glacial_seed_table) do
+		world:for_each_neighbor(ti, function(nti)
+			if glacial_seed_table[nti] ~= nil then return end
+			old_layer[nti] = true
+			distance_from_edge[nti] = 1
+		end)
+	end
+	-- world:for_each_tile_by_elevation_for_waterflow(function(ti)
+	-- 	if glacial_seed[ti] then return end
+
+	-- 	local has_seed_neighbour = false
+	-- 	world:for_each_neighbor(ti, function(nti)
+	-- 		if not glacial_seed[nti] then return end
+	-- 		has_seed_neighbour = true
+	-- 	end)
+
+	-- 	if has_seed_neighbour then
+	-- 		old_layer[ti] = true
+	-- 		distance_from_edge[ti] = 1
+	-- 	end
+	-- end)
+
+	local duration = love.timer.getTime() - start
+	print("[glacial-formation] find_glacial_perimeter: " .. tostring(duration * 1000) .. "ms")
+	local count = 0
+	for _ in pairs(old_layer) do
+		count = count + 1
+	end
+	print("Old Layer: " .. count)
+end
+
+local function reset_variables(world)
+	world:fill_ffi_array(glacial_seed, false)
+	glacial_seed_table = {}
+	world:fill_ffi_array(ice_flow, 0)
+	world:fill_ffi_array(ice_moved, 0)
+	world:fill_ffi_array(distance_from_edge, 0)
 end
 
 ---@param world World
@@ -62,6 +143,9 @@ local function process_age(world, age_type)
 	local is_ice_age = age_type == AGE_TYPES.ice_age
 
 	create_glacial_start_locations(world, is_ice_age)
+	find_glacial_perimeter(world)
+
+	reset_variables(world)
 end
 
 function gm.run(world)
@@ -80,6 +164,7 @@ function gm.run(world)
 	world:fill_ffi_array(glacial_seed, false)
 	world:fill_ffi_array(ice_flow, 0)
 	world:fill_ffi_array(ice_moved, 0)
+	world:fill_ffi_array(distance_from_edge, 0)
 
 	process_age(world, AGE_TYPES.ice_age)
 	process_age(world, AGE_TYPES.game_age)
