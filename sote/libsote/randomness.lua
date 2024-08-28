b1e83f38219048ac01bd82ddcb8712d8a5be4a89
dsecrieru <danysecrieru@yahoo.com>
2024-08-28 16:27:31 +0200

    added get/set for seed and state; minor refactoring

--- sote/libsote/randomness.lua
+++ sote/libsote/randomness.lua
@@ -12,6 +12,26 @@ function rand:new(seed)
 	return new
 end
 
+---@return number
+function rand:get_seed()
+	return self.rng:getSeed()
+end
+
+---@return string
+function rand:get_state()
+	return self.rng:getState()
+end
+
+---@param seed number
+function rand:set_seed(seed)
+	self.rng:setSeed(seed)
+end
+
+---@param state string
+function rand:set_state(state)
+	self.rng:setState(state)
+end
+
 ---@return number [0 .. 1)
 function rand:random()
 	return self.rng:random()
@@ -40,15 +60,11 @@ end
 local ffi = require("ffi")
 local bit = require("bit")
 
-ffi.cdef[[
-typedef uint32_t uint;
-]]
-
 ---@param input number
 ---@return number?
 function rand.pcg_hash(input)
-	local state = ffi.new("uint", input * 747796405 + 2891336453)
-	local word = ffi.new("uint", bit.bxor(state, bit.rshift(state, bit.bxor(bit.rshift(state, 28), 4))) * 277803737)
+	local state = ffi.new("uint32_t", input * 747796405 + 2891336453)
+	local word = ffi.new("uint32_t", bit.bxor(state, bit.rshift(state, bit.bxor(bit.rshift(state, 28), 4))) * 277803737)
 	return tonumber(bit.bxor(word, bit.rshift(word, 22)))
 end
 
