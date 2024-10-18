f280138e49a18c077f49ced6460c5f1148f3fe43
dsecrieru <danysecrieru@yahoo.com>
2024-10-18 19:25:06 +0200

    enabled debug for next stage(s) port

--- sote/libsote/debug-control-panel.lua
+++ sote/libsote/debug-control-panel.lua
@@ -1,12 +1,12 @@
 local dcp = {}
 
-dcp.map_tiles_from_file = false -- this will load cube world tile IDs mapping to hex from a file, as it's faster than computing them from lat lon
-dcp.use_sote_climate_data = false -- climate model was ported from SotE, but with some changes; this will enable import of original SotE climate data from a csv file, to aid in debugging/validating port
-dcp.use_sote_ice_data = false -- this will enable import of original SotE ice data from a csv file, to aid in debugging/validating port
+dcp.map_tiles_from_file = true -- this will load cube world tile IDs mapping to hex from a file, as it's faster than computing them from lat lon
+dcp.use_sote_climate_data = true -- climate model was ported from SotE, but with some changes; this will enable import of original SotE climate data from a csv file, to aid in debugging/validating port
+dcp.use_sote_ice_data = true -- this will enable import of original SotE ice data from a csv file, to aid in debugging/validating port
 dcp.align_to_sote_coords = dcp.use_sote_climate_data or dcp.use_sote_ice_data -- this will align hex world storage to match the order from original SotE, very useful when debugging/validating port
 
 dcp.glaciation = {
-	align_rng = false -- this will align RNG seed to match original SotE glaciation
+	align_rng = true -- this will align RNG seed to match original SotE glaciation
 }
 
 dcp.save_maps = false -- this will export maps to PNG
@@ -27,7 +27,6 @@ dcp.maps_selection = {
 -- seed = 6618 -- tiny islands?
 -- seed = 49597 -- interesting looking one, huge northern ice cap (with lua climate model)
 -- seed = 91170 -- huge lake
--- dcp.fixed_seed = nil
--- dcp.fixed_seed = 12177
+dcp.fixed_seed = 12177
 
 return dcp
