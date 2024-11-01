8bbd4159a508a5e40f410a03d5feb253a95747c8
dsecrieru <danysecrieru@yahoo.com>
2024-11-01 20:44:38 +0100

    refactor to get rid of for_each_neighbor

--- sote/libsote/post-tectonic.lua
+++ sote/libsote/post-tectonic.lua
@@ -5,20 +5,22 @@ local elevations_fixed = 0
 local function fix_elevation(world)
 	local fixed_elevation = false
 
-	world:for_each_tile(function(i, _)
-		local elevation = world.elevation[i]
+	world:for_each_tile(function(ti)
+		local elevation = world.elevation[ti]
 
-		world:for_each_neighbor(i, function(ni)
-			local neighbor_elevation = world.elevation[ni]
+		local num_neighs = world:neighbors_count(ti)
+		for i = 0, num_neighs - 1 do
+			local nti = world.neighbors[ti * 6 + i]
+			local neighbor_elevation = world.elevation[nti]
 
 			if neighbor_elevation == elevation then
 				elevation = elevation + world.rng:random() * 0.001
-				world.elevation[i] = elevation
+				world.elevation[ti] = elevation
 
 				fixed_elevation = true
 				elevations_fixed = elevations_fixed + 1
 			end
-		end)
+		end
 	end)
 
 	return fixed_elevation
